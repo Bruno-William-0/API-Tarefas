e744a9e0e1e0d644810236473f27a70fde924892
Bruno-William-0 <bruwill04@gmail.com>
2025-02-27 13:29:01 -0300

    Listando task

--- src/controllers/task/task.controller.ts
+++ src/controllers/task/task.controller.ts
@@ -17,4 +17,22 @@ export default class TaskController {
     res.status(201).json(task)
     return
   }
+
+  static async index (req: Request, res: Response) {
+    const tasks = await Task.find()
+ res.json(tasks)
+ return
+  }
+
+  static async show (req: Request, res: Response) {
+    const { id } = req.params
+
+    if(!id || isNaN(Number(id))) {
+    res.status(400).json({ error: 'O id é obrigatório' })
+    }
+
+    const task = await Task.findOneBy({id: Number(id)})
+    res.json(task)
+    return
+  }
 }
